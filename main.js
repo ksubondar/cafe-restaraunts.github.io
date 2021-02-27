8d514345ad972e7c2544c0b29f52289a27128478
ksubondar <79761397+ksubondar@users.noreply.github.com>
2021-02-28 01:30:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".menuToggle").click(function() {
+      $(this).toggleClass("active");
+      $('.menu').toggleClass("active");
+      $('.menu').slideToggle(300, function(){
+        if($(this).css('display') === "none"){
+          $(this).removeAttr('style');
+        }
+      });
+    });
+  });
+  
